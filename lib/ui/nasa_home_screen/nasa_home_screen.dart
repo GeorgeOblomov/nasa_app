8bf09fc0f6d3d08e31d44b5e35964dd621f3a820
Egor Grischenkov <grischenkov01@gmail.com>
2022-02-09 22:27:35 +0300

    initial commit

---
+++ lib/ui/nasa_home_screen/nasa_home_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NasaInfoScreen extends StatelessWidget {
+  const NasaInfoScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
