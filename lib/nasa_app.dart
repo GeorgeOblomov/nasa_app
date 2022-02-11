5e32434971236127b8c3e25ea297d48ed1e9b51d
Egor Grischenkov <grischenkov01@gmail.com>
2022-02-11 10:09:23 +0300

    change progress indicator color

--- lib/nasa_app.dart
+++ lib/nasa_app.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:nasa_app/ui/nasa_home_screen/nasa_home_screen.dart';
+import 'package:nasa_app/utils/app_colors.dart';
 
 class NasaApp extends StatelessWidget {
-
   const NasaApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'NASA API app',
+      theme: ThemeData(
+        progressIndicatorTheme:
+            const ProgressIndicatorThemeData(color: progressIndicatorColor),
+      ),
       home: NasaHomeScreen(),
     );
   }
--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -4,3 +4,5 @@ final tabItemGradientColors = [
   const Color(0xFF42275a),
   const Color(0xFF734b6d),
 ];
+
+const progressIndicatorColor = Color(0xFF42275a);
