717309ab13c0718e94a3f04c1170461e373c3563
Egor Grischenkov <grischenkov01@gmail.com>
2022-02-14 15:57:54 +0300

    fix tests

--- test/about_screen/about_app_screen_test.dart
+++ test/about_screen/about_app_screen_test.dart
@@ -4,6 +4,6 @@ import 'package:nasa_app/ui/nasa_home_screen/navigation_bar_pages/nasa_about_pag
 void main() {
   testGoldens('About app page golden test', (tester) async {
     await tester.pumpWidgetBuilder(const NasaAboutPage());
-    await screenMatchesGolden(tester, 'about_page');
+    await multiScreenGolden(tester, 'about_page');
   });
 }
