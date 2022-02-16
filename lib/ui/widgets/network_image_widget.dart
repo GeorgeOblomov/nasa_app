ce561458ff8d9125a89a68120df449913e3862e8
Egor Grischenkov <grischenkov01@gmail.com>
2022-02-16 14:06:57 +0300

    create image screen and plug photo_view package

--- lib/ui/widgets/network_image_widget.dart
+++ lib/ui/widgets/network_image_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:nasa_app/extensions/localization_extension.dart';
+import 'package:nasa_app/ui/nasa_image_screen/nasa_image_screen.dart';
+import 'package:nasa_app/utils/app_colors.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class NetworkImageWidget extends StatelessWidget {
@@ -9,53 +11,76 @@ class NetworkImageWidget extends StatelessWidget {
   final double? height;
   final double? width;
 
-  const NetworkImageWidget(
-      {required this.url, required this.isTest, this.padding, this.height, this.width, Key? key,})
-      : super(key: key);
+  const NetworkImageWidget({
+    required this.url,
+    required this.isTest,
+    this.padding,
+    this.height,
+    this.width,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: padding,
-      height: height,
-      width: width,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        color: imageBackground,
-      ),
-      clipBehavior: Clip.hardEdge,
-      child: Stack(
-        children: [
-          const Center(child: CircularProgressIndicator()),
-          Center(
-            child: FadeInImage(
-              placeholder: isTest
-                  ? const AssetImage(
-                'assets/placeholder_image.jpeg',
-              )
-                  : MemoryImage(kTransparentImage) as ImageProvider,
-              image: NetworkImage(url),
-              imageErrorBuilder: (_,
-                  __,
-                  ___,) {
-                return Container(
-                  color: imageBackground,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Icon(
-                        Icons.error,
-                        color: Colors.red,
+    return GestureDetector(
+      onTap: () => _onImageTap(context, url),
+      child: Container(
+        padding: padding,
+        height: height,
+        width: width,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: imageBackground,
+        ),
+        clipBehavior: Clip.hardEdge,
+        child: Stack(
+          children: [
+            const Center(child: CircularProgressIndicator()),
+            Center(
+              child: Hero(
+                tag: url,
+                child: FadeInImage(
+                  placeholder: isTest
+                      ? const AssetImage(
+                          'assets/placeholder_image.jpeg',
+                        )
+                      : MemoryImage(kTransparentImage) as ImageProvider,
+                  image: NetworkImage(url),
+                  imageErrorBuilder: (
+                    _,
+                    __,
+                    ___,
+                  ) {
+                    return Container(
+                      color: imageBackground,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Icon(
+                            Icons.error,
+                            color: Colors.red,
+                          ),
+                          const SizedBox(height: 8),
+                          Text(context.localizations.errorText),
+                        ],
                       ),
-                      SizedBox(height: 8),
-                      Text(errorText),
-                    ],
-                  ),
-                );
-              },
+                    );
+                  },
+                ),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _onImageTap(BuildContext context, String url) {
+    Navigator.push(context,
+      MaterialPageRoute<void>(
+        builder: (context) => NasaImageScreen(
+          url: url,
+        ),
       ),
     );
   }
