a04cd1720a3578bc43b3d541efc7ebdcd780ed44
Egor Grischenkov <grischenkov01@gmail.com>
2022-02-18 12:54:54 +0300

    create favorite screen

---
+++ lib/ui/nasa_favorite_screen/nasa_favorite_model.dart
@@ -0,0 +1,17 @@
+import 'package:elementary/elementary.dart';
+import 'package:flutter/material.dart';
+import 'package:nasa_app/db/i_database_manager.dart';
+
+class NasaFavoriteModel extends ElementaryModel {
+  final ValueNotifier<List<String>> favoritePhotos = ValueNotifier([]);
+
+  final IDatabaseManager _databaseManager;
+
+  NasaFavoriteModel(this._databaseManager);
+
+  Future<void> getFavorites() async {
+    final favorites = await _databaseManager.getFavorites();
+
+    favoritePhotos.value = favorites;
+  }
+}
